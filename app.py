0e33a85143620a4d93605d016c841925cf4b3eae
Piyush Mokadam <piyu268@gmail.com>
2022-11-09 17:08:34 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+
+@app.route('/api/employees', methods=['GET'])
+def get_employees():
+    employees = EmpModel.query.all() 
+    data = [
+    {
+        "id" : emp.id,
+        "name" : emp.name,
+        "designation" : emp.designation, "gender" : emp.gender,
+        "salary" : emp.salary,
+        "joining__date" : emp. joining__date,
+    } for emp in employees]
+    return jsonify({'employees': data})
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
