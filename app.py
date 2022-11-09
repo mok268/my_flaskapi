ce964f16a9f897e85c38a696de254a419168ad7e
Piyush Mokadam <piyu268@gmail.com>
2022-11-09 18:04:37 +0530

    Commented dynamic code and added static json response

--- app.py
+++ app.py
@@ -9,15 +9,19 @@ def hello_world():
 
 @app.route('/api/employees', methods=['GET'])
 def get_employees():
-    employees = EmpModel.query.all() 
-    data = [
-    {
-        "id" : emp.id,
-        "name" : emp.name,
-        "designation" : emp.designation, "gender" : emp.gender,
-        "salary" : emp.salary,
-        "joining__date" : emp. joining__date,
-    } for emp in employees]
+    employees = EmpModel.query.all()
+    data = [{
+        "id": 1,
+        "name": "Abc"
+    }] 
+    # data = [
+    # {
+    #     "id" : emp.id,
+    #     "name" : emp.name,
+    #     "designation" : emp.designation, "gender" : emp.gender,
+    #     "salary" : emp.salary,
+    #     "joining__date" : emp. joining__date,
+    # } for emp in employees]
     return jsonify({'employees': data})
 
 
